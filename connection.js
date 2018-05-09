6989b928aeea1148a19790f1f7698275dcbcf569
Ben Iofel <iofelben@gmail.com>
2018-05-09 15:35:03 -0400

    login

---
+++ connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/beanbook")
+
+const conn = mongoose.connection;
+
+module.exports = {
+  mongoose,
+  db: new Promise((resolve, reject) => {
+    conn.on('error', (err) => {
+      console.error('connection error: ', err);
+      reject(err);
+    });
+    conn.once('open', () => resolve(conn));
+  })
+};
