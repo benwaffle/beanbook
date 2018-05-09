7873140cb74a8cf204a301f5e42cbb9fbe71ae56
Philip Fishbein <phil@fishbe.in>
2018-05-09 14:41:23 -0400

    Fix data files

--- data/beans.js
+++ data/beans.js
@@ -3,58 +3,40 @@ const uuid = require("uuid/v4");
 
 module.exports = {
   getAllBeans() {
-    Bean.find(function (err, beans) {
-      if (err) throw err;
-      return beans;
-    });
+    return Bean.find().exec();
   },
-  getBeanById(id) {
-    Bean.find({ _id: id }, 
-      function (err, bean) {
-        if (err) throw err;
-        return bean;
-      }
-    );
+  getBeanById(_id) {
+    return Bean.findOne({ _id }).exec();
   },
-  addBean(userId, title, type, description) {
-    if (!userId) throw "There must be a creator for this bean";
+  addBean(creatorId, title, type, description) {
+    if (!creatorId) throw "There must be a creator for this bean";
     if (!title) throw "Title cannot be blank";
     if (!type) throw "Type cannot be blank";
     if (!description) throw "Description cannot be blank";
     
-    let newBean = new Bean({
-      creatorId: userId,
-      title: title,
+    const newBean = new Bean({
+      creatorId,
+      title,
       _id: uuid(),
-      type: type,
-      description: description,
+      type,
+      description,
       comments: [],
       timestamp: new Date().toISOString()
     });
 
-    newBean.save(function (err, bean) {
-      if (err) throw err;
-      return bean;
-    });
+    return newBean.save().exec();
   },
-  removeBean(id) {
-    Bean.remove({ _id: id }, function (err) {
-      if (err) throw err;
-    });
+  removeBean(_id) {
+    return Bean.remove({ _id }).exec()
   },
-  updateBean(id, userId, title, type, description) {
-    let updatedBean = {
-      creatorId: userId,
-      title: title,
-      type: type,
-      description: description,
+  updateBean(_id, creatorId, title, type, description) {
+    const updatedBean = {
+      creatorId,
+      title,
+      type,
+      description,
     };
-    Bean.findOneAndUpdate({ _id: id }, updatedBean, 
-      function (err, bean) {
-        if (err) throw err;
-        return bean;
-      }
-    );
+    return Bean.findOneAndUpdate({ _id }, updatedBean).exec()
   },
   getCommentById(id) {
     //TODO
