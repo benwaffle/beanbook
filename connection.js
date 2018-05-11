a9f2b8bcb427bda93a3a7bf0bd3dbe33cce0ecbc
Ben Iofel <ben@flow.io>
2018-05-11 15:54:06 -0400

    add seed.js

--- connection.js
+++ connection.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/beanbook")
+mongoose.connect('mongodb://localhost:27017/beanbook');
 
 const conn = mongoose.connection;
 
 module.exports = {
   mongoose,
   db: new Promise((resolve, reject) => {
-    conn.on('error', (err) => {
+    conn.on('error', err => {
       console.error('connection error: ', err);
       reject(err);
     });
     conn.once('open', () => resolve(conn));
+    conn.on('close', () => reject('closed'));
   })
 };
